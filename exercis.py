a0b4914ae7d5e6a1b588db7c1845e44a1d195099
andreyrobot <kr4t0s.vieir4@gmail.com>
2025-07-01 11:00:17 -0300

    Add files via upload

---
+++ exercis.py
@@ -0,0 +1,13 @@
+nome = input("Qual o nome?: ")
+print(f" obrigado por usar nosso progama {nome}, que belo nome👌")
+idade = input(f"quantos anos você tem {nome}?:")
+
+print("OK, vamos começar!!!")
+
+n = int(input("fale o numero INTEIRO que você queira saber: "))
+
+if n % 2 == 0:
+
+    print(f"Seu numero é par: {n}")
+else:
+    print(f" Seu numero é impar")
